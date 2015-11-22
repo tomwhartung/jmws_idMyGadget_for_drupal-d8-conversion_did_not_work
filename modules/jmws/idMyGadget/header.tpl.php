cc89ca1847d16e696fc52c97664d90e872e7f05e
Tom W Hartung <tomwhartung@gmail.com>
2015-11-21 19:20:45 -0700

    Adding in the actual Drupal code.

---
+++ modules/jmws/idMyGadget/header.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * can I define a header.tpl.php ?
+ * It doesn't look like this works
+ *
+ * CRUFT ALERT: this was checked in by accident; consider deleting
+ *
+ */
+?>
+<div>
+  <p>Exp paragraph for header 1.</p>
+</div>
