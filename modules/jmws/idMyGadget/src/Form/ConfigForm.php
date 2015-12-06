453212a28c397fb8c0e7c0dc1743b028ad7aec02
Tom W Hartung <tomwhartung@gmail.com>
2015-12-06 16:34:41 -0700

    First pass at updating ConfigForm.php as needed.

--- modules/jmws/idMyGadget/src/Form/ConfigForm.php
+++ modules/jmws/idMyGadget/src/Form/ConfigForm.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace Drupal\hugs\Form;
+namespace Drupal\idMyGadget\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 class ConfigForm extends ConfigFormBase {
   public function getFormId() {
-    return 'hug_config';
+    return 'idMyGadget_config';
   }
 
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('hugs.settings');
+    $config = $this->config('idMyGadgets.settings');
 
-    $form['default_count'] = [
-      '#type' => 'number',
-      '#title' => $this->t('Default hug count'),
-      '#default_value' => $config->get('default_count'),
-    ];
+//    $form['default_count'] = [
+//      '#type' => 'number',
+//      '#title' => $this->t('Default hug count'),
+//      '#default_value' => $config->get('default_count'),
+//    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -25,12 +25,12 @@ class ConfigForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $config = $this->config('hugs.settings');
+    $config = $this->config('idMyGadget.settings');
     $config->set('default_count', $form_state->getValue('default_count'));
     $config->save();
   }
 
   public function getEditableConfigNames() {
-    return ['hugs.settings'];
+    return ['idMyGadget.settings'];
   }
 }
