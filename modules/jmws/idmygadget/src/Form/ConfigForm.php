c72bbf2e0ceaa320c53c325701482faf1101b576
Tom W Hartung <tomwhartung@gmail.com>
2015-12-19 13:33:27 -0700

    Updated ConfigForm.php with code to set the idmygadget_gadget_detector .

--- modules/jmws/idmygadget/src/Form/ConfigForm.php
+++ modules/jmws/idmygadget/src/Form/ConfigForm.php
@@ -7,17 +7,32 @@ use Drupal\Core\Form\FormStateInterface;
 
 class ConfigForm extends ConfigFormBase {
   public function getFormId() {
-    return 'idMyGadget_config';
+    return 'idmygadget_config';
   }
 
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('idMyGadgets.settings');
+    $config = $this->config('idmygadget.settings');
 
-//    $form['default_count'] = [
-//      '#type' => 'number',
-//      '#title' => $this->t('Default hug count'),
-//      '#default_value' => $config->get('default_count'),
-//    ];
+   $supported_gadget_detectors = array(
+      'detect_mobile_browsers',   // note that this is used as the default throughout
+      'mobile_detect',
+      'tera_wurfl',
+      'no_detection'      // defaults to desktop (allows for isolating responsive behavior)
+   );
+   //
+   // Add a section to the module's Settings screen that contains
+   // radio buttons allowing the admin to set the device detector.
+   // This shows up under Configuration -> IdMyGadget -> Gadget Detector
+   //
+   $form['idmygadget_gadget_detector'] = array(
+      '#type' => 'radios',
+      '#title' => t('Gadget Detector (ConfigForm)'),
+      // '#default_value' => $supported_gadget_detectors[0],
+	  '#default_value' => $config->get('idmygadget_gadget_detector'),
+      '#options' => $supported_gadget_detectors,
+      '#description' => $this->t('Select the 3rd party device detector to use for this site.'),
+      '#required' => TRUE,
+   );
 
     return parent::buildForm($form, $form_state);
   }
@@ -25,8 +40,8 @@ class ConfigForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $config = $this->config('idMyGadget.settings');
-    $config->set('default_count', $form_state->getValue('default_count'));
+    $config = $this->config('idmygadget.settings');
+    $config->set('idmygadget_gadget_detector', $form_state->getValue('idmygadget_gadget_detector'));
     $config->save();
   }
 
